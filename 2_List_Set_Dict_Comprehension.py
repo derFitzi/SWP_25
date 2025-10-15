af25c2bf903a2385b5d5936cd6160aedf9b7b9dd
Lukas <137492918+derFitzi@users.noreply.github.com>
2025-10-15 17:55:07 +0200

    Implement dictionary comprehension for alphabets
    Added functions for dictionary comprehension of alphabets.

--- 2_List_Set_Dict_Comprehension.py
+++ 2_List_Set_Dict_Comprehension.py
@@ -1,3 +1,5 @@
+import string
+
 # Buchstaben aus gemischten String herausholen
 def listComprehension(zahlenWortMix):
     return[zeichen for zeichen in zahlenWortMix if zeichen.isalpha()]
@@ -10,11 +12,23 @@ def setComprehension(satz):
 def dictComprehension(bis):
     return{zahl+1:"ungerade" if (zahl+1)%2 else "gerade" for zahl in range(bis)}
 
+"""
+def dictComprehensionAlphabet(alphabet):
+    return{buchstabe: ord(buchstabe)-64 for buchstabe in alphabet}
+"""
+# Wandelt ein geordnetes Alphabet als String in ein Dictionary mit "Index" aus, erster Buchstabe hat immer Index 1
+def dictComprehensionAlphabet(alphabet):
+    return{buchstabe: ord(buchstabe)-(ord(alphabet[0])-1) for buchstabe in alphabet}
 
 def main():
     print("List Comprehension: ", listComprehension("12h2343a23ll983475o3459082345"))
     print("Set Comprehension: ", setComprehension("Ich bin ein Satz mit vielen Wörtern und einigen Wiederholungen"))
     print("Dict Comprehension: ", dictComprehension(10))
+
+    print(dictComprehensionAlphabet("ABCDEFGHIJKLMNOPQRATUVWXYZ"))
+    print(dictComprehensionAlphabet(string.ascii_uppercase))    # nicht Hardcoded
+    teilweise_alphabet = ''.join([buchstabe for buchstabe in string.ascii_uppercase][6:24])  # Liste aus String, diese slicen und dann zu String joinen
+    print(dictComprehensionAlphabet(teilweise_alphabet))
 
 if __name__ == "__main__":
     main()
